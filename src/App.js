dfc63cb7084b6ca655fead463c881f729484d93c
Arunsamuvel <arun15ee03@gmail.com>
2024-09-02 20:24:37 +0530

    3rd component for incomeExpense setup created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Balance from "./Component/Balance";
 import Header from "./Component/Header";
+import IncomeExpense from "./Component/IncomeExpense";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div>
+      <div className="container">
         <Balance />
+        <IncomeExpense />
       </div>
     </div>
   );
