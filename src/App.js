a505cd4023dc4a2b11762305d00311685375ece1
Arunsamuvel <arun15ee03@gmail.com>
2024-09-02 20:16:35 +0530

    2nd component for balance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Balance from "./Component/Balance";
 import Header from "./Component/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div>
+        <Balance />
+      </div>
     </div>
   );
 }
