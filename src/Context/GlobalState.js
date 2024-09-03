ba03bd4a062147f3e915ccae8f1c9d0d9e45645b
Arunsamuvel <arun15ee03@gmail.com>
2024-09-03 12:23:07 +0530

    added the add transaction setup using context

--- src/Context/GlobalState.js
+++ src/Context/GlobalState.js
@@ -1,5 +1,6 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
+import { type } from "@testing-library/user-event/dist/type";
 
 // initial state
 const initialState = {
@@ -19,11 +20,25 @@ export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
   // action
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
 
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
       }}
     >
       {children}
